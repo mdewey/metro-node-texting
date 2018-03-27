3efa994cd53ed483eb2b72bb79163798d77fcb4a
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-26 23:06:34 -0400

    sample test app for UI working

--- test-app/public/main.js
+++ test-app/public/main.js
@@ -1,6 +1,22 @@
+const BASE_URL ="https://metro-node-texting.herokuapp.com/"
+
 angular.module("main", []).controller("mainController", ['$scope', "$http", ($scope, $http) => {
   $http({
     method:"GET", 
-    url:""
+    url:BASE_URL + "ping"
+  }).then(resp => {
+    console.log({resp});
+  })
+
+
+  $http({
+    method:"POST",
+    url:BASE_URL + "send/message",
+    data:JSON.stringify({
+      number:"8144219298"
+    })
+  }).then(resp => {
+    console.log({resp});
+    console.log(resp.data)
   })
 }])
